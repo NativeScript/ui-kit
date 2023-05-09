f2e4a73f1fe7a6a5c76d04bc588e74d80724f4c0
Nathan Walker <walkerrunpdx@gmail.com>
2023-05-09 09:38:27 -0700

    feat: flutter (#3)

---
+++ packages/flutter/index.d.ts
@@ -0,0 +1,10 @@
+import { FlutterCommon } from './common';
+export { FlutterChannelType, IFlutterMessage } from './common';
+
+export function init();
+
+export function configureFlutterEngineGroup();
+
+export declare class Flutter extends FlutterCommon {}
+
+export declare class FlutterDelegate extends UIResponder {}
